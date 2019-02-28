0551eb77164301adb2973df6af69b92148114244
Parth Sarin <parthsarin@gmail.com>
2019-02-27 19:33:47 -0800

    Add functionality for inputting and outputting np arrays

--- IO.py
+++ IO.py
@@ -5,16 +5,25 @@ import numpy as np
 import warnings
 
 class Movement:
+	"""Captures movement features for the car.
+
+	Acceptable syntax:
+		Movement(front=True, right=True)
+		Movement(False, False, True, False) # left, right, front, back
+		Movement(np.array([0, 1, 0, 0])) # left, right, front, back
+		Movement(np_array=np.array([0, 1, 0, 1]))
+
+	Note: If you want to initialize with a regular array instead of a numpy
+	array, just unpack it. E.g.:
+		movement = [False, False, True, False]
+		Movement(*movement)
+
+		Initializing with an np_array can be done either by passing in an
+		np_array positionally or with np_array = (array).
+
+	"""
 	def __init__(self, *args, **kwargs):
-		"""Initializes the Movement class. This can be done in two ways. Either with
-		four boolean values or with an np_array.
-
-			Note: If you want to initialize with a regular array, just unpack it. E.g.:
-				movement = [False, False, True, False]
-				Movement(*movement)
-
-				Initializing with an np_array can be done either by passing in an
-				np_array positionally or with np_array = (array).
+		"""Initializes the Movement class.
 
 		:left: Whether the car moves left.
 		:right: Whether the car moves right.
@@ -22,62 +31,20 @@ class Movement:
 		:back: Whether the car moves back.
 		:np_array: A numpy array from which to read movement values.
 		"""
-		# If np_array is in the keyword args
-		if 'np_array' in kwargs:
-			if arg.shape == (4,):
-				left, right, front, back = \
-					self._np_init(kwargs['np_array'])
-			else:
-				warnings.warn('Your numpy array does not have the correct shape. Should be (4,).', SyntaxWarning)
-				print("Not accepting the keyword argument.")
+		# Parse the arguments
+		left, right, front, back = _get_params(args, kwargs)
 
-		# If any of the positionals are numpy arrays
-		for arg in args:
-			if type(arg) is np.ndarray:
-				if arg.shape == (4,):
-					left, right, front, back = \
-						self._np_init(arg)
-				else:
-					warnings.warn('Your numpy array does not have the correct shape. Should be (4,).', SyntaxWarning)
-					print("Not accepting the variadic positional argument.")
-
-		# If the first four are boolean
-		if all(map(lambda x: type(x) is bool, args[:4])):
-			left, right, front, back = \
-				args[:4]
-		
-		# Have we defined the variables yet?
-		try:
-			left
-		except NameError:
-			# Ya done goofed.
-			left, right, front, back = \
-				[False]*4
-			warnings.warn("Couldn't find any reasonable movement arguments. Taking all directions to be false.", SyntaxWarning)
-
-		# Initialize with the four parameters
+		# Moving in opposite directions goes nowhere
 		if left and right:
 			left = right = False
 		if front and back:
 			front = back = False
 
+		# Initialize on the class
 		self.left = left
 		self.right = right
 		self.front = front
 		self.back = back
-
-	def _np_init(self, np_array):
-		"""Initializes the Movement class from a numpy array.
-
-			Note: The syntax is the following:
-				movement = np.array([0, 0, 1, 0]) # left, right, front, back
-				movement = Movement(np_array = movement)
-				# or...
-				movement = Movement(movement)
-
-		:np_array: A numpy array from which to read movement values.
-		"""
-		return tuple(map(bool, np_array))
 
 	def asNpArray(self, normalized = False):
 		"""Returns the instance of this class as a numpy array.
@@ -91,3 +58,58 @@ class Movement:
 			output = output / sum(output)
 
 		return output
+
+	def _get_params(self, args, kwargs):
+		"""Parses `args` and `kwargs` for the left, right, front, and back
+		variables.
+		"""
+		# The variables should be false if undefined
+		left = right = front = back = False
+
+		# We can get the parameters in many ways...
+		# ...if np_array is in the keyword args
+		if 'np_array' in kwargs:
+			arg = kwargs['np_array']
+			if arg.shape == (4,):
+				left, right, front, back = self._np_init(kwargs['np_array'])
+			else:
+				warnings.warn('Your numpy array does not have the correct shape. Should be (4,).', SyntaxWarning)
+				print("Not accepting the keyword argument {}.".format(str(arg)))
+
+		# ...if any of the positionals are numpy arrays
+		for arg in args:
+			if type(arg) is np.ndarray:
+				if arg.shape == (4,):
+					left, right, front, back = self._np_init(arg)
+				else:
+					warnings.warn('Your numpy array does not have the correct shape. Should be (4,).', SyntaxWarning)
+					print("Not accepting the variadic positional argument {}.".format(arg))
+
+		# ...if the first four are boolean
+		if all(map(lambda x: type(x) is bool, args[:4])):
+			left, right, front, back = args[:4]
+
+		# ...or if any of the parameters are in kwargs
+		if 'left' in kwargs:
+			left = kwargs['left']
+		if 'right' in kwargs:
+			right = kwargs['left']
+		if 'front' in kwargs:
+			front = kwargs['front']
+		if 'back' in kwargs:
+			back = kwargs['back']
+		
+		return left, right, front, back
+
+	def _np_init(self, np_array):
+		"""Initializes the Movement class from a numpy array.
+
+			Note: The syntax is the following:
+				movement = np.array([0, 0, 1, 0]) # left, right, front, back
+				movement = Movement(np_array = movement)
+				# or...
+				movement = Movement(movement)
+
+		:np_array: A numpy array from which to read movement values.
+		"""
+		return tuple(map(bool, np_array))
