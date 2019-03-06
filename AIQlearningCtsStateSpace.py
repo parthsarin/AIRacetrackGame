0eade4c28ca9dd849ee96c16c2db86d75a06dc51
Jade Lintott <lintottjade@gmail.com>
2019-03-05 21:25:55 -0800

    Add learning rate and fix clustering issue
    Fix clustering by dividing by sum of weights and use the learning rate to weight random noise that we add to the expected reward for a particular state.

--- AIQlearningCtsStateSpace.py
+++ AIQlearningCtsStateSpace.py
@@ -5,25 +5,30 @@ from pathlib import Path # for remembering the table
 import numpy as np # for math
 import collections # for storing (state, action) pairs
 import pickle # for storing the table
+import random
 import errors
 
 import IO
 
 MEMORY_FILE = 'memory/qlearning.mem'
+REWARD_RANGE = 100
+DEFAULT_LEARNING_RATE = .465
 QLearningData = collections.namedtuple('QLearningData', ['state', 'action'])
 
-def process(state, NUM_TO_DIR):
+def process(state, NUM_TO_DIR, LEARNING_RATE = DEFAULT_LEARNING_RATE):
 	"""Decides which direction to move based on the current state.
 
 	:state: An instance of IO.State which describes the current state
 	of the game.
 	:NUM_TO_DIR: A dictionary that translates single-digit directions
 	into a dictionary of booleans containing directional information.
+	:LEARNING_RATE: A number between 0 and 1 that represents how fast
+	the algorithm should learn.
 	"""
 	approximations = {}
 	for direction in NUM_TO_DIR:
 		# Approximate the value of each action
-		approximations[direction] = approximateValue(state, IO.Movement(**NUM_TO_DIR[direction]))
+		approximations[direction] = approximateValue(state, IO.Movement(**NUM_TO_DIR[direction]), LEARNING_RATE)
 
 	print("Approximated ", approximations)
 
@@ -33,7 +38,7 @@ def process(state, NUM_TO_DIR):
 		return IO.Movement(front=True)
 	return IO.Movement(**NUM_TO_DIR[optimalDirection[0]])
 
-def approximateValue(state, action):
+def approximateValue(state, action, LEARNING_RATE):
 	"""Approximates the expected reward for taking a given
 	action from a given state.
 
@@ -41,18 +46,26 @@ def approximateValue(state, action):
 	of the game.
 	:action: An instance of IO.Movement which describes the action
 	the program is considering
+	:LEARNING_RATE: A number between 0 and 1 that represents how fast
+	the algorithm should learn.
 	"""
 	# Create a falloff function to weight the points we have info for
 	kDecay = 1 # width of one standard deviation
 	falloff_func = lambda x: np.exp(- np.pi * (x/kDecay) ** 2) # exponential decay
 
-	pred_value = 0
+	# Create the values that will be modified in the loop
+	pred_value_num = 0
+	pred_value_denom = 0
 
 	# Create a numpy vector representing the current state (shape: (11,))
 	state_vector = buildMLVector(state.distances, state.velocity, state.velocity_magnitude)
 
 	# Load the table
 	table = loadQTable()
+
+	# Create random noise
+	random_val = (random.random()*200 - 100) * (1 - LEARNING_RATE) / (len(table)+1)
+
 	if table:
 		# loop over data points
 		for func_value in table:
@@ -63,11 +76,15 @@ def approximateValue(state, action):
 				
 				# Distance between the two points
 				distance = np.linalg.norm(state_vector-func_value_state_vector)
-				pred_value += falloff_func(distance) * table[func_value]
+				pred_value_num += falloff_func(distance) * table[func_value]
+				pred_value_denom += falloff_func(distance)
 
-		return pred_value
+		if pred_value_denom != 0:
+			return pred_value_num / pred_value_denom + random_val
+		else:
+			return random_val
 	else:
-		return 0
+		return random_val
 
 def buildMLVector(distances, velocity, magnitude):
 	"""Rescale distances & velocity and combine them into a single vector.
