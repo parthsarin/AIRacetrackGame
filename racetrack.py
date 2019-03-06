a2b960e968a23e8fb8858de008b5f62c4a84a20d
ColinNorick <colin.norick@gmail.com>
2019-03-05 17:52:29 -0800

    Added basic movement, not finished.

--- racetrack.py
+++ racetrack.py
@@ -5,10 +5,19 @@ Basic shell from MarquisLP Github.
 Revision history:
 @Antonio & @Colin (2/16/19) created file
 @Colin (2/16/19 5:38pm) added a basic shell for a pygame loop
+@Colin (3/5/19 5:38pm) added basic movement (not finished but getting there)
 """
+import os
+import pygame
 import sys
 import pygame
+import utils
+import IO
+import Map
+import numpy as np
 from pygame.locals import *
+from math import tan, radians, degrees, copysign
+from pygame.math import Vector2
 # Import additional modules here.
 
 
@@ -19,15 +28,42 @@ degree = 0
 blue = (0,0,255)
 
 class car:
-    def __init__(self, rotation, location, points, width, height):
-        self.rotation = 0
-        self.location = 0
-        self.points = (80, 0, 40, 30)
-        self.width = 10
-        self.height = 10
+    """Initializes car calss.
 
-    def draw_rect(self):
-        pygame.draw.rect(game_screen,blue,(200,150,100,50))
+        :x the x position.
+        :y the y postiion.
+        :angle: Angle of the car’s body, relative to the world, in degrees
+        :length: length of car chassis
+        :max_steering: maximum steering value in degrees
+        :max_acceleration: value of acceleration, in meters per second squared
+        :acceleration: current acceleration
+        :steering:  current steering value, in degrees. Negative values mean that the wheels are turned to the right and positive – to the left.
+        """
+    def __init__(self, x, y, angle=0.0, length=4, max_steering=30, max_acceleration=5.0):
+        self.position = Vector2(x, y)
+        self.velocity = Vector2(0.0, 0.0)
+        self.angle = angle
+        self.length = length
+        self.max_acceleration = max_acceleration
+        self.max_steering = max_steering
+
+        self.acceleration = 0.0
+        self.steering = 0.0
+
+     def update(self, dt):
+        self.velocity += (self.acceleration * dt, 0)
+        self.velocity.x = max(-self.max_velocity, min(self.velocity.x, self.max_velocity))
+
+        if self.steering:
+            turning_radius = self.length / tan(radians(self.steering))
+            angular_velocity = self.velocity.x / turning_radius
+        else:
+            angular_velocity = 0
+
+        self.position += self.velocity.rotate(-self.angle) * dt
+        self.angle += degrees(angular_velocity) * dt
+
+
 
 
 def pygame_modules_have_loaded():
@@ -41,8 +77,39 @@ def pygame_modules_have_loaded():
         success = False
 
     return success
+s
+def move_car (mov_array, car) :
 
+        if pressed[pygame.K_UP]: # moving up
+                if car.velocity.x < 0:
+                car.acceleration = car.brake_deceleration
+                else:
+                car.acceleration += 1 * dt
+        elif pressed[pygame.K_DOWN]:
+            if car.velocity.x > 0:
+                    car.acceleration = -car.brake_deceleration
+            else:
+                car.acceleration -= 1 * dt
+        elif pressed[pygame.K_SPACE]:
+            if abs(car.velocity.x) > dt * car.brake_deceleration:
+                car.acceleration = -copysign(car.brake_deceleration, car.velocity.x)
+            else:
+                car.acceleration = -car.velocity.x / dt
+        else:
+            if abs(car.velocity.x) > dt * car.free_deceleration:
+                car.acceleration = -copysign(car.free_deceleration, car.velocity.x)
+            else:
+                if dt != 0:
+                    car.acceleration = -car.velocity.x / dt
+        car.acceleration = max(-car.max_acceleration, min(car.acceleration, car.max_acceleration))
 
+        if pressed[pygame.K_RIGHT]:
+            car.steering -= 30 * dt
+        elif pressed[pygame.K_LEFT]:
+                car.steering += 30 * dt
+        else:
+            car.steering = 0
+        car.steering = max(-car.max_steering, min(car.steering, car.max_steering))    
        
 
 pygame.mixer.pre_init(44100, -16, 2, 512)
