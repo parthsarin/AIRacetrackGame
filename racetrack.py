f48070a17a32dfd288073fc0a3b7069da7a7de1c
ColinNorick <colin.norick@gmail.com>
2019-03-07 16:41:39 -0800

    moved car class to its own file

--- racetrack.py
+++ racetrack.py
@@ -13,6 +13,7 @@ import os
 import pygame
 import sys
 import utils
+import Car
 import IO
 import Map
 import numpy as np
@@ -30,52 +31,7 @@ blue = (0,0,255)
 
 
 
-class Car:
-    """Initializes CAR class.
-        :x the x position.
-        :y the y postiion.
-        :angle: Angle of the car’s body, relative to the world, in degrees
-        :length: length of car chassis
-        :width: width of car chassis
-        :max_steering: maximum steering value in degrees
-        :max_acceleration: value of acceleration, in meters per second squared
-        :acceleration: current acceleration
-        :steering:  current steering value, in degrees. Negative values mean that the wheels are turned to the right and positive – to the left.
-        """
-    def __init__(self, x, y, angle=90.0, length=40, width=60, max_steering=450, max_acceleration=10.0):
-        self.position = Vector2(x, y)
-        self.velocity = Vector2(0.0, 0.0)
-        self.angle = angle
-        self.length = length
-        self.width = width
-        self.max_acceleration = max_acceleration
-        self.max_steering = max_steering
-        self.max_velocity = 30
-        self.brake_deceleration = 10
-        self.free_deceleration = 2
 
-        self.acceleration = 0.0
-        self.steering = 0.0
-
-    def update(self, dt):
-        self.velocity += (self.acceleration * dt, 0)
-        self.velocity.x = max(-self.max_velocity, min(self.velocity.x, self.max_velocity))
-
-        if self.steering:
-            turning_radius = self.length / tan(radians(self.steering))
-            angular_velocity = self.velocity.x / turning_radius
-        else:
-            angular_velocity = 0
-
-        #debug statement
-        #print("Angular Velocity:", angular_velocity,"  steering:" ,self.steering)
-
-        self.position += self.velocity.rotate(-self.angle) * dt
-        self.angle += degrees(angular_velocity) * dt
-
-    def draw(self):
-
-        pygame.draw.rect(game_screen,blue,pygame.Rect((self.position[0],self.position[1]),(self.width, self.length)))
 
 
 
